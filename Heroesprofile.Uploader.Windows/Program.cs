9078354f48cdf31168d2f43773ddd2403d0dbcf9
Chris <cmillhou@gmail.com>
2020-03-30 09:14:01 -0400

    fix:  Changed replay storage location to cause all users to check for uploads again

--- Heroesprofile.Uploader.Windows/Program.cs
+++ Heroesprofile.Uploader.Windows/Program.cs
@@ -36,8 +36,8 @@ namespace Heroesprofile.Uploader.Windows
             }
 
             // Move files from old locations
-            if (File.Exists($@"{App.AppDir}\..\replays.xml") && !File.Exists($@"{App.SettingsDir}\replays.xml")) {
-                File.Move($@"{App.AppDir}\..\replays.xml", $@"{App.SettingsDir}\replays.xml");
+            if (File.Exists($@"{App.AppDir}\..\replays_v2.xml") && !File.Exists($@"{App.SettingsDir}\replays_v2.xml")) {
+                File.Move($@"{App.AppDir}\..\replays.replays_v2", $@"{App.SettingsDir}\replays_v2.xml");
             }
             if (File.Exists($@"{App.AppDir}\..\last.config") && !File.Exists($@"{App.SettingsDir}\last.config")) {
                 File.Move($@"{App.AppDir}\..\last.config", $@"{App.SettingsDir}\last.config");

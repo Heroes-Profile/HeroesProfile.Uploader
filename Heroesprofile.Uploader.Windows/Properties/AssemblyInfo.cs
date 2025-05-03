33d838f218b8501eeba6ac868335e9acfaa884de
Zemill <33404722+Zemill@users.noreply.github.com>
2025-05-03 16:59:52 -0400

    Update AssemblyInfo.cs

--- Heroesprofile.Uploader.Windows/Properties/AssemblyInfo.cs
+++ Heroesprofile.Uploader.Windows/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Heroesprofile Uploader")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
